f9ae11fe8b48c973c1b28bfd12603fb61e40126e
Luis Avila <avilaluisfernando@hotmail.com>
2024-07-16 18:18:19 -0300

    se termino la tabla y el array que contiene el numero de apariciones de la suma de los dados

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,6 +1,6 @@
 //Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.
 
-let suma = [];
+const suma = [];
 
 for (let i = 0; i < 50; i++) {
   let dado1 = Math.floor(Math.random() * 6) + 1;
@@ -11,9 +11,34 @@ for (let i = 0; i < 50; i++) {
 }
 console.log(suma);
 
+const sumaOrdenada = suma.sort(function(a, b){return a - b});
+console.log(sumaOrdenada);
+
+let recuento = 1;
+const apariciones = [];
+const combinaciones = [];
+
+for (let j = 0; j < sumaOrdenada.length; j++){
+    if (sumaOrdenada[j+1] === sumaOrdenada [j]) {
+      recuento++;
+    } else {
+      combinaciones.push(sumaOrdenada[j]);
+      apariciones.push(recuento);
+      recuento = 1
+    }
+}
+console.log(combinaciones);
+console.log(apariciones);
+
 document.write(`<table><tbody>`)
 document.write(`<tr>`)
 document.write(`<td> Suma </td>`)
 document.write(`<td> Apariciones </td>`)
 document.write(`</tr>`)
-document.write(`</td></td>`)
+for(let i=0; i<11; i++){
+document.write(`<tr>`)
+  document.write(`<td> ${combinaciones[i]} </td>`)
+  document.write(`<td> ${apariciones[i]} </td>`)
+  document.write(`</tr>`)
+}
+document.write(`</tbody></table>`)
